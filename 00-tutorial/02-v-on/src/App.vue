8808f5c0a7b0a43ce8bca8097cfd8453bc15c862
zyj <1305696765@qq.com>
2024-07-16 22:01:46 +0800

    tutorial::v-on: 事件监听
    Signed-off-by: zyj <1305696765@qq.com>

---
+++ 00-tutorial/02-v-on/src/App.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { ref } from 'vue';
+
+const count = ref(0);
+
+function increment() {
+  count.value++;
+}
+
+const count1 = ref(1000);
+function decrement() {
+  count1.value--;
+}
+
+const dynamicClass = ref('a-none');
+
+</script>
+
+<template>
+  <div>
+    <!-- 使此按钮生效 -->
+    <button v-on:click="increment">Count is: {{ count }}</button>
+    <!-- 简写语法 -->
+    <button @click="decrement">Count is: {{ count1 }}</button>
+    <a href="https://www.baidu.com" :class="dynamicClass">Baidu</a>
+  </div>
+</template>
+
+<style>
+button, a {
+  display: block;
+  cursor: pointer;
+}
+
+.a-none {
+  text-decoration: none;
+}
+
+.a-none:hover {
+  background-color: black;
+  color: white;
+}
+
+</style>
