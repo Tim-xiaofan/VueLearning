145714d2586343dbd7ad763da8d754cdbb979750
zyj <1305696765@qq.com>
2024-07-16 21:39:35 +0800

    tutorial::Attribute 绑定
    Signed-off-by: zyj <1305696765@qq.com>

---
+++ 00-tutorial/03-v-bind/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from 'vue'
+
+const titleClass = ref('title')
+const dynamicId = ref('dynamic');
+</script>
+
+<template>
+  <div v-bind:id="dynamicId"> <!-- 动态id -->
+    <!-- mustache 语法 (即双大括号) 只能用于文本插值。为了给 attribute 绑定一个动态值，需要使用 v-bind 指令 -->
+    <h1 v-bind:class="titleClass">Make me red</h1> <!-- 此处添加一个动态 class 绑定 -->
+  </div>
+</template>
+
+<style>
+.title {
+  color: red;
+}
+
+#dynamic {
+  background-color: aliceblue;
+  border: groove black 3px;
+  border-radius: 5px;
+  padding: 1em;
+}
+</style>
