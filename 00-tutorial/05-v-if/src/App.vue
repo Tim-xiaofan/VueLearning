3d6bb13fa09f2a89e79c1e35f6f3edee3ce98816
zyj <1305696765@qq.com>
2024-07-18 20:29:53 +0800

    tutorial::v-if: 条件渲染
    Signed-off-by: zyj <1305696765@qq.com>

---
+++ 00-tutorial/05-v-if/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue';
+
+const awesome = ref(true);
+
+function toggle() {
+  awesome.value = !awesome.value;
+}
+
+</script>
+
+<template>
+  <button @click="toggle">Toggle</button>
+  <!-- 条件渲染：在两个 h1 之前切换 -->
+  <h1 v-if="awesome">Vue is awesome!</h1>
+  <h1 v-else>Oh no 😢</h1>
+</template>
