50cfcf7a2affbf2fb4ff0418a818d5e477843303
zyj <1305696765@qq.com>
2024-07-19 20:19:29 +0800

    tutorial::ref attribute: 声明周期和模板引用
    Signed-off-by: zyj <1305696765@qq.com>

---
+++ 00-tutorial/08-lifecycle-and-templateref/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+// 注意这个 ref 使用 null 值来初始化。
+// 这是因为当 <script setup> 执行时，DOM 元素还不存在。
+// 模板引用 ref 只能在组件挂载后访问
+const pElementRef = ref(null);
+
+onMounted(() => {
+  // 此时组件已挂载
+  pElementRef.value.textContent += ' World';
+});
+
+</script>
+
+<template>
+  <!-- 个特殊的 ref attribute -->
+  <p ref="pElementRef"> Hello </p>
+</template>
+
+<style scoped>
+</style>
