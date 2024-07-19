0f904614643491f39036af91a2145cb076aa826d
zyj <1305696765@qq.com>
2024-07-19 21:49:14 +0800

    tutorial::components: 子组件
    Signed-off-by: zyj <1305696765@qq.com>

---
+++ 00-tutorial/10-components/src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+// 导入子组件
+import ChildComp from './components/ChildComp.vue';
+</script>
+
+<template>
+  <div>
+    <h1>A Parent Component!</h1>
+    <!-- 使用子组件 -->
+    <ChildComp />
+  </div>
+</template>
+
+<style scoped>
+div {
+  background-color: aqua;
+  border: groove green 3px;
+  border-radius: 10px;
+  padding: 1em;
+}
+</style>
---
+++ 00-tutorial/10-components/src/components/ChildComp.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+const count = ref(0);
+</script>
+
+<template>
+    <div>
+        <h1>A Child Component!</h1>
+        <button @click="count++"> Count is: {{ count }}</button>
+    </div>
+</template>
+
+<style scoped>
+div {
+    border: solid black 2px;
+    border-radius: 5px;
+    background-color: aliceblue;
+    padding: 1em;
+}
+</style>
