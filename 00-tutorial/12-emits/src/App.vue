861b701ce4c67aa43fd929e8a4b381d39b431962
zyj <1305696765@qq.com>
2024-07-20 10:56:56 +0800

    tutorial::emits: 子组件向父组件触发事件
    Signed-off-by: zyj <1305696765@qq.com>

---
+++ 00-tutorial/12-emits/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import ChildComp from './components/ChildComp.vue';
+
+const childMsg = ref('No child msg yet');
+</script>
+
+<template>
+  <ChildComp @response="(msg:string) => { childMsg = msg; }"/>
+  <p> {{ childMsg }}</p>
+</template>
+
+<style scoped></style>
---
+++ 00-tutorial/12-emits/src/components/ChildComp.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+// 声明触发的事件
+const emit = defineEmits(['response']);
+
+const bRef = ref(null);
+
+onMounted(() => {
+    console.log('bRef: ', bRef.value);
+    const handler = (e) => {
+        let b = e.srcElemt || e.target;
+        console.log('b: ', b);
+        // 带参数触发
+        emit('response', 'Hello from child');
+        b.removeEventListener('click', handler);
+    };
+    bRef.value.addEventListener('click', handler);
+});
+
+</script>
+
+<template>
+    <button ref="bRef">emit</button>
+</template>
+
+<style scoped></style>
