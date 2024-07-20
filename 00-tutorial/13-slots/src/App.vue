8da6253345aa90666ae1d94b978542ef6dfba90f
zyj <1305696765@qq.com>
2024-07-20 12:07:23 +0800

    tutorial::slots: 父组件还可以通过插槽 (slots) 将模板片段传递给子组件
    Signed-off-by: zyj <1305696765@qq.com>

---
+++ 00-tutorial/13-slots/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import ChildComp from './components/ChildComp.vue';
+
+const msg = ref('from parent');
+</script>
+
+<template>
+  <ChildComp> 
+    {{ msg }} : This is some slot content!
+  </ChildComp>
+</template>
+
+<style scoped></style>
---
+++ 00-tutorial/13-slots/src/components/ChildComp.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <!-- 在子组件的模板中 -->
+    <slot>Fallback content</slot>
+</template>
+
+<style scoped></style>
